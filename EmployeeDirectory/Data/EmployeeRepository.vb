d3d0731ed81ce2beb43c0a4ea7b5d9e5e0cf22db
Sheldon Paden <sheldon_kinse@yahoo.com>
2018-01-15 10:44:11 -0600

    commit #1 for EmployeeRepository changes

---
+++ EmployeeDirectory/Data/EmployeeRepository.vb
@@ -0,0 +1,27 @@
+﻿Imports BASD.Helper
+
+Imports System.Configuration
+Imports System.Data.SqlClient
+
+Public Class EmployeeRepository
+    Inherits BaseRepository
+
+    Public Sub New()
+        MyBase.New(ConfigurationManager.ConnectionStrings("EmployeeDirectoryConnectionString").ConnectionString.Decrypt)
+    End Sub
+
+    Public Overrides Function GetData(ByVal entity As IEntity) As BASD.Helper.IEntity
+        Dim _tempParamList = New List(Of SqlParameter)()
+        _tempParamList.Add(New SqlParameter("@OracleID", entity.Id))
+
+        Dim _tempEmployeeList = MyBase.List("[uspGetEmployee]", _tempParamList.ToArray())
+
+        Return _tempEmployeeList.SingleMapToEntity(Of Employee)().SingleOrDefault()
+
+    End Function
+
+    Public Overrides Function GetListData() As System.Collections.Generic.IEnumerable(Of BASD.Helper.IEntity)
+        Dim _tempEmployeeList = MyBase.List("uspGetEmployeeList", Nothing)
+        Return _tempEmployeeList.SingleMapToEntity(Of Employee)()
+    End Function
+End Class
